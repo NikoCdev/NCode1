5a6220a71fcafd7ac2baec3ce1143399a4305134
NikoCdev <rusyakoval25@gmail.com>
2023-08-23 22:43:02 +0400

    1
    1

---
+++ Фильмы/Фильмы/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MovieApp.Models;
+using System.Diagnostics;
+
+namespace UploadFile.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ApplicationContext _context;
+
+        public HomeController(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View("~/Views/Home/Index.cshtml", _context.Movies.ToList());
+        }
+
+        public IActionResult GetMovie(int id)
+        {
+            
+            var movie = _context.Movies.FirstOrDefault(m => m.Id == id);
+
+            if (movie == null)
+            {
+                return NotFound(); 
+            }
+
+            return View(movie); 
+        }
+    }
+}
---
+++ Фильмы/Фильмы/Models/MovieModel.cs
@@ -0,0 +1,13 @@
+﻿namespace MovieApp.Models
+{
+    public class MovieModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Director { get; set; }
+        public string Genre { get; set; }
+        public int ReleaseYear { get; set; }
+        public string PosterUrl { get; set; }
+        public string Description { get; set; }
+    }
+}
