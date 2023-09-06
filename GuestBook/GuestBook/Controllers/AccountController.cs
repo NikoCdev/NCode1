ffbe9bdf57e9781b5f3b7b406ba67e2c86499bb9
NikoCdev <rusyakoval25@gmail.com>
2023-09-06 19:22:53 +0400

    Гостевая книга дз v1
    Первое домашнее задание на гостевую книгу

---
+++ GuestBook/GuestBook/Controllers/AccountController.cs
@@ -0,0 +1,156 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Gbook.Models;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Gbook.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserContext _context;
+
+        public AccountController(UserContext context)
+        {
+            _context = context;
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Login(LoginModel logon)
+        {
+            if (ModelState.IsValid)
+            {
+                if (_context.Users.ToList().Count == 0)
+                {
+                    ModelState.AddModelError("", "Wrong login or password!");
+                    return View(logon);
+                }
+                var users = _context.Users.Where(a => a.Login == logon.Login);
+                if (users.ToList().Count == 0)
+                {
+                    ModelState.AddModelError("", "Wrong login or password!");
+                    return View(logon);
+                }
+                var user = users.First();
+                string? salt = user.Salt;
+
+                //переводим пароль в байт-массив  
+                byte[] password = Encoding.Unicode.GetBytes(salt + logon.Password);
+
+                //создаем объект для получения средств шифрования  
+                var md5 = MD5.Create();
+
+                //вычисляем хеш-представление в байтах  
+                byte[] byteHash = md5.ComputeHash(password);
+
+                StringBuilder hash = new StringBuilder(byteHash.Length);
+                for (int i = 0; i < byteHash.Length; i++)
+                    hash.Append(string.Format("{0:X2}", byteHash[i]));
+
+                if (user.Pwd != hash.ToString())
+                {
+                    ModelState.AddModelError("", "Wrong login or password!");
+                    return View(logon);
+                }
+                HttpContext.Session.SetString("Login", user.Login);
+                HttpContext.Session.SetInt32("UserId", user.Id);
+                return RedirectToAction("Index", "Home");
+            }
+            return View(logon);
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Register(RegisterModel reg)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = new User();
+                user.Login = reg.Login;
+
+                byte[] saltbuf = new byte[16];
+
+                RandomNumberGenerator randomNumberGenerator = RandomNumberGenerator.Create();
+                randomNumberGenerator.GetBytes(saltbuf);
+
+                StringBuilder sb = new StringBuilder(16);
+                for (int i = 0; i < 16; i++)
+                    sb.Append(string.Format("{0:X2}", saltbuf[i]));
+                string salt = sb.ToString();
+
+                //переводим пароль в байт-массив  
+                byte[] password = Encoding.Unicode.GetBytes(salt + reg.Password);
+
+                //создаем объект для получения средств шифрования  
+                var md5 = MD5.Create();
+
+                //вычисляем хеш-представление в байтах  
+                byte[] byteHash = md5.ComputeHash(password);
+
+                StringBuilder hash = new StringBuilder(byteHash.Length);
+                for (int i = 0; i < byteHash.Length; i++)
+                    hash.Append(string.Format("{0:X2}", byteHash[i]));
+
+                user.Pwd = hash.ToString();
+                user.Salt = salt;
+                _context.Users.Add(user);
+                _context.SaveChanges();
+                return RedirectToAction("Login");
+            }
+
+            return View(reg);
+        }
+
+        [HttpGet]
+        public IActionResult SendMessage()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task <IActionResult> SendMessage(string messageText)
+        {
+            if (ModelState.IsValid)
+            {
+                if (!string.IsNullOrEmpty(messageText))
+                {
+                    // Получить идентификатор текущего пользователя из сессии
+                    var userId = HttpContext.Session.GetInt32("UserId");
+                    var userLogin = HttpContext.Session.GetString("Login");
+                    var user = await _context.Users.SingleOrDefaultAsync(u => u.Login == userLogin);
+
+                    if (userId.HasValue)
+                    {
+                        // Создать объект сообщения
+                        var message = new Message
+                        {
+                            User = user,
+                            MessageText = messageText,
+                            MessageDate = DateTime.Now
+                        };
+
+                        // Добавить сообщение в контекст данных и сохранить изменения
+                        _context.Messages.Add(message);
+                        _context.SaveChanges();
+                    }
+                }
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+
+
+}
---
+++ GuestBook/GuestBook/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Gbook.Models;
+using System.Linq;
+
+public class HomeController : Controller
+{
+    private readonly UserContext _context;
+
+    public HomeController(UserContext context)
+    {
+        _context = context;
+    }
+
+    public IActionResult Index()
+    {
+        var messages = _context.Messages
+            .Include(m => m.User) 
+            .ToList();
+
+        return View(messages);
+    }
+
+    public ActionResult Logout()
+    {
+        HttpContext.Session.Clear();
+        return RedirectToAction("Login", "Account");
+    }
+
+}
+
