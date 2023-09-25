16bbcf225fe4c3f9194bfccd998ba068873f68ee
NikoCdev <108999620+NikoCdev@users.noreply.github.com>
2023-09-25 17:37:31 +0400

    Музыкальный портал репозиторий
    Музыкальный портал на IRepository

---
+++ Music_Portal_IRepo/Music_Portal_DepInjection/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Gbook.Repository;
+
+public class HomeController : Controller
+{
+    private readonly IRepository _repository;
+
+    public HomeController(IRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public IActionResult Index()
+    {
+        var genresList = _repository.GetAllGenresAsync().Result;
+        var songsList = _repository.GetAllSongsAsync().Result;
+
+        ViewBag.DataLists = new { Genres = genresList, Songs = songsList };
+
+        return View();
+    }
+
+    public ActionResult Logout()
+    {
+        HttpContext.Session.Clear();
+        return RedirectToAction("Login", "Account");
+    }
+
+    public async Task<IActionResult> Users()
+    {
+        var userList = await _repository.GetAllUsersAsync();
+        return View(userList);
+    }
+}
---
+++ Music_Portal_IRepo/Music_Portal_DepInjection/Repository/IRepository.cs
@@ -0,0 +1,27 @@
+﻿using Gbook.Models;
+namespace Gbook.Repository
+{
+
+    public interface IRepository
+    {
+        Task<List<Song>> GetAllSongsAsync();
+        Task<Song> GetSongByIdAsync(int id);
+        Task AddSongAsync(Song message);
+        Task UpdateSongAsync(Song message);
+        Task DeleteSongAsync(Song message);
+
+        Task<List<User>> GetAllUsersAsync();
+        Task<User> GetUserByIdAsync(int id);
+        Task<User> GetUserByLoginAsync(string login);
+        Task AddUserAsync(User user);
+        Task UpdateUserAsync(User user);
+        Task DeleteUserAsync(User user);
+        
+        Task<List<Genre>> GetAllGenresAsync();
+        Task<Genre> GetGenreByIdAsync(int id);       
+        Task AddGenreAsync(Genre user);
+        Task UpdateGenreAsync(Genre user);
+        Task DeleteGenreAsync(Genre user);
+    }
+
+}
